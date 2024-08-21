933978be311b4bbc78ac65b69a405f2928040f75
onkardh32 <onkardhamma18@gmail.com>
2024-08-21 11:07:45 +0530

    Onkar[added few basic programs]

---
+++ first_program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace first_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate();
+        }
+
+        public static void Calculate()
+        {
+            int num = 10;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The given number " + num + " is an even number");
+            }
+            else
+            {
+                Console.WriteLine("The given number " + num + " is Odd number");
+            }
+
+        }
+                    
+    }
+}
