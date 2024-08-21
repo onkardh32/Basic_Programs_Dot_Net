42970a3935b96e6f2d1dad124ac8bfcc33d0c8b5
Onkar Dhamma <119420508+onkardh32@users.noreply.github.com>
2024-08-22 01:16:45 +0530

    Add files via upload

---
+++ first_program/CheckOddEven.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace first_program
+{
+    internal class CheckOddEven
+    {
+        public static void EvenOddDemo()
+        {
+            Console.WriteLine("Hey input the number : ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The given input number " + num + " is even !");
+            }
+            else
+            {
+                Console.WriteLine("The number " + num + " is odd ! ");
+            }
+        }
+    }
+}
