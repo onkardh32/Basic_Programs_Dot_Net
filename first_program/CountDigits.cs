1daeb9776f24c77778524596efcb74073c3d28b5
Onkar Dhamma <119420508+onkardh32@users.noreply.github.com>
2024-08-22 10:05:06 +0530

    Add files via upload

---
+++ first_program/CountDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace first_program
+{
+    class CountDigits
+    {
+        public static void DigitCountDemo()
+        {
+            Console.Write("Enter the Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int count = 0; int temp = number;
+
+            while (temp > 0)
+            {
+
+                count += 1;
+                temp = temp / 10;
+            }
+
+            Console.WriteLine("The total digits  : "+number +" has is " + count);
+        }
+    }
+}
